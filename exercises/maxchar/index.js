853df081bf28de1f3a8e761f58ca8ec6bb390482
emzys <emzysk@gmail.com>
2025-07-21 00:18:47 +0900

    maxchar solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chaMap = {}
+    let maxChar = ''
+    let maxCount = 0
+
+    for (let char of str) {
+        chaMap[char] = chaMap[char] + 1 || 1
+    }
+
+    for (let char in chaMap) {
+        if (chaMap[char] > maxCount) {
+            maxCount = chaMap[char]
+            maxChar = char
+        }
+    }
+
+    return maxChar
+}
 
 module.exports = maxChar;
