fa6876796167ade7c12b927bdd37c08989fd16ea
emzys <emzysk@gmail.com>
2025-07-24 10:40:33 +0900

    anagram solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,37 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+
+// function anagrams(stringA, stringB) {
+//     const cleanedA = stringA.replace(/[^\w]/g, '').toLowerCase();
+//     const cleanedB = stringB.replace(/[^\w]/g, '').toLowerCase();
+
+//     if (cleanedA.length !== cleanedB.length) {
+//         return false;
+//     }
+
+//     const charCount = {};
+
+//     for (let char of cleanedA) {
+//         charCount[char] = (charCount[char] || 0) + 1;
+//     }
+
+//     for (let char of cleanedB) {
+//         if (!charCount[char]) {
+//             return false;
+//         }
+//         charCount[char]--;
+//     }
+
+//     const sum = Object.values(charCount).reduce((acc, count) => acc + count, 0);
+//     return sum === 0;
+// }
 
 module.exports = anagrams;
